a003662791141dcc5e1bca5174b9802bafbb1d8c
cyberkamath <kamath.r@icloud.com>
2026-09-10 00:38:34 +0200

    fix typo

---
+++ url_shortener/analytics_service/src/main/java/sh/kamath/analytics_service/AnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package sh.kamath.analytics_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsServiceApplication.class, args);
+	}
+
+}
---
+++ url_shortener/analytics_service/src/test/java/sh/kamath/analytics_service/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.kamath.analytics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
